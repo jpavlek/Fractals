69904915afcec7bb2ceb2c2283d6648e62ce41d7
jpavlek <jakov.pavlek@gmail.com>
2021-04-15 22:37:55 +0200

    Add Timer class to support code performance measuring, profiling and optimization.

---
+++ Timer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <chrono>
+
+class Timer
+{
+public:
+	static int calls_;
+
+private:
+	std::chrono::high_resolution_clock::time_point start_;
+	std::chrono::high_resolution_clock::time_point end_;
+	std::chrono::duration<double> elapsedTime_;
+
+public:
+	Timer() noexcept;
+	void start() noexcept;
+	void end() noexcept;
+};
+
