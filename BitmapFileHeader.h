4834dbf33b033f25a2e45012eed2f917eaf2e178
jpavlek <jakov.pavlek@gmail.com>
2021-04-15 21:48:04 +0200

    Fixed order of elements bitmaps needed for bmp file reading.

--- BitmapFileHeader.h
+++ BitmapFileHeader.h
@@ -4,8 +4,8 @@
 
 struct BitmapFileHeader
 {
+	char header_[2]{ 'B', 'M' };
 	int32_t filesize_{ -1 };
 	int32_t reserved_{ 0 };
 	int32_t dataOffset_{ -1 };
-	char header_[2]{'B', 'M'};
 };
--- BitmapInfoHeader.h
+++ BitmapInfoHeader.h
@@ -6,12 +6,12 @@ struct BitmapInfoHeader
 	int32_t headerSize_{ 40 };
 	int32_t width_{ 0 };
 	int32_t heigth_{ 0 };
+	int16_t planes_{ 1 };
+	int16_t bitsPerPixel_{ 24 };
 	int32_t compression_{ 0 };
 	int32_t dataSize_{ 0 };
 	int32_t horizontalResolution_{ 2400 };
 	int32_t verticalResolution_{ 2400 };
 	int32_t colors_{ 0 };
 	int32_t importantColors_{ 0 };
-	int16_t planes_{ 1 };
-	int16_t bitsPerPixel_{ 24 };
 };
