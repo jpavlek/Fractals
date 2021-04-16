a83116f17540b7ee4e0bb31e10e66c880852e780
jpavlek <jakov.pavlek@gmail.com>
2021-04-16 04:30:11 +0200

    Add ProcessorFeatureSet class to support checking for CPU features and its Enhanced Instruction Sets Support.

---
+++ ProcessorFeatureSet.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+#include <bitset>
+#include <vector>
+
+class ProcessorFeatureSet
+{
+private:
+	std::array<int, 4> cpui;
+	int nIds_;
+	int nExIds_;
+	std::bitset<32> f_1_ECX_{ 0 };
+	std::bitset<32> f_1_EDX_{ 0 };
+
+	std::bitset<32> f_7_EBX_{ 0 };
+	std::bitset<32> f_7_ECX_{ 0 };
+
+	std::bitset<32> f_81_ECX_{ 0 };
+	std::bitset<32> f_81_EDX_{ 0 };
+	std::vector<std::array<int, 4>> data_;
+	std::vector<std::array<int, 4>> extdata_;
+	std::string vendor_;
+	std::string brand_;
+
+public:
+	static void checkProcessorFeatures();
+public:
+	ProcessorFeatureSet();
+};
+
