037dc75ff3cfb13dd00e43dc3434c2d7eecaaf9c
jpavlek <jakov.pavlek@gmail.com>
2021-04-16 04:49:25 +0200

    Add main function within Fractals.cpp file. Checks for CPU model and enhanced instruction set features. Creates up to 60 color palettes with up to 2560 colors from a 24RGB-bit color space ad saves them in ./Palettes folder (if it exists). Creates a Mandelbrot fractal BMP image of size 8192x8192 with up to 2048 iterations and measures total processing time.

---
+++ Fractals.cpp
@@ -0,0 +1,36 @@
+// Fractals.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#pragma pack(2)
+#include "Mandelbrot.h"
+#include <iostream>
+#include "ColorPaletteCreator.h"
+#include "Timer.h"
+
+#include <stdio.h>
+#include "ProcessorFeatureSet.h"
+
+#ifdef _WIN32
+#include <intrin.h>
+#endif
+
+int main()
+{
+	Timer timer;
+	timer.start();
+
+	ProcessorFeatureSet::checkProcessorFeatures();
+
+	int const width = 2 * 2 * 2048;
+	int const height = 2 * 2 * 2048;
+	int const maxIterations = 2048;
+
+	//ColorPaletteCreator::createAllPalettesTest();
+
+	ColorPalette colorPalette = ColorPaletteCreator::createPalette(ColorPalettesClass::RGBCube3, 1);
+
+	Fractals::Mandelbrot mandelbrot(width, height, colorPalette, maxIterations);
+
+	std::cout << "Done.\n";
+	timer.end();
+	return 0;
+}
