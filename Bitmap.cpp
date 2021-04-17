69d199ee5f87401f1d510d09bbf5c004e3bdd6c3
Jakov Pavlek <jakov.pavlek@gmail.com>
2021-04-17 22:55:27 +0200

    Add namespace FileSystem to Bitmap.cpp.

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -6,64 +6,74 @@
 #include <fstream>
 #include <iostream>
 
-Bitmap::Bitmap(int width, int height) : 
-	width_(width), 
-	height_(height), 
-	pPixels_(new uint8_t[width * height * 3])
+namespace Colors
 {
+	struct Color;
 }
 
-void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
+namespace FileSystem
 {
-	uint8_t* pPixel = pPixels_.get();
-	pPixel += (y * 3) * width_ + (x * 3);
-	pPixel[0] = blue;
-	pPixel[1] = green;
-	pPixel[2] = red;
-}
 
-void Bitmap::setPixel(int x, int y, Color color)
-{
-	uint8_t red = color.red_;
-	uint8_t green = color.green_;
-	uint8_t blue = color.blue_;
-	setPixel(x, y, red, green, blue);
-}
-
-bool Bitmap::write(const std::string& filename)
-{
-	BitmapFileHeader fileHeader;
-	BitmapInfoHeader infoHeader;
-
-	fileHeader.dataOffset_ = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
-
-	fileHeader.filesize_ = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + width_ * height_ * 3;
-	infoHeader.width_ = width_;
-	infoHeader.heigth_ = height_;
-
-	std::ofstream file;
-	file.open(filename, std::ios::out | std::ios::binary);
-
-	if (!file)
+	Bitmap::Bitmap(int width, int height) :
+		width_(width),
+		height_(height),
+		pPixels_(new uint8_t[width * height * 3])
 	{
-		std::cout << "\nFile: " << filename << " could not be opened...\n";
-		return false;
 	}
 
-	file.write((char*)&fileHeader, sizeof(fileHeader));
-	file.write((char*)&infoHeader, sizeof(infoHeader));
-	file.write((char*)pPixels_.get(), width_ * height_ * 3);
-
-	file.close();
-
-	if (!file)
+	void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
 	{
-		return false;
+		uint8_t* pPixel = pPixels_.get();
+		pPixel += (y * 3) * width_ + (x * 3);
+		pPixel[0] = blue;
+		pPixel[1] = green;
+		pPixel[2] = red;
 	}
 
-	return true;
-}
+	void Bitmap::setPixel(int x, int y, const Colors::Color& color)
+	{
+		uint8_t red = color.red_;
+		uint8_t green = color.green_;
+		uint8_t blue = color.blue_;
+		setPixel(x, y, red, green, blue);
+	}
 
-Bitmap::~Bitmap()
-{
+	bool Bitmap::write(const std::string& filename)
+	{
+		BitmapFileHeader fileHeader;
+		BitmapInfoHeader infoHeader;
+
+		fileHeader.dataOffset_ = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+		fileHeader.filesize_ = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + width_ * height_ * 3;
+		infoHeader.width_ = width_;
+		infoHeader.heigth_ = height_;
+
+		std::ofstream file;
+		file.open(filename, std::ios::out | std::ios::binary);
+
+		if (!file)
+		{
+			std::cout << "\nFile: " << filename << " could not be opened...\n";
+			return false;
+		}
+
+		file.write((char*)&fileHeader, sizeof(fileHeader));
+		file.write((char*)&infoHeader, sizeof(infoHeader));
+		file.write((char*)pPixels_.get(), width_ * height_ * 3);
+
+		file.close();
+
+		if (!file)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	Bitmap::~Bitmap()
+	{
+	}
+
 }
