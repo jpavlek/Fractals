a00146b5484822836337be83591bc1dfb22f174a
Jakov Pavlek <jakov.pavlek@gmail.com>
2021-04-17 19:34:28 +0200

    Add comment on how to enable AVX-512F - Advanced Vector Extensions 512 (/arch:AVX512)  enhanced instruction set.

--- Mandelbrot.h
+++ Mandelbrot.h
@@ -46,7 +46,16 @@ namespace Fractals
 		void calculateMultiThreads(ComplexRectangle& complexRectangle) noexcept;
 		void calculateMultiThreads(ComplexRectangle& complexRectangle, int threadCountBase) noexcept;
 		void calculate(const ComplexRectangle& complexRectangle, const Rectangle<int>& screenRectangle) noexcept;
-		
+
+/**
+*	By default Advanced Vector Extensions 2 (/arch:AVX2) enhanced instruction set (AVX2) should be active in the MS Visual Studio (2019) project.
+*   However, if you are using a different IDE or a different version of MS VS, it might not be activated.
+*	To accelerate the execution in the case your CPU supports AVX2, enable it by going to:
+*	Project -> Fractals Properties -> Configuration Properties -> C/C++ -> Code Generation -> Enable Enhanced Instruction Set -> Advanced Vector Extensions 2 (/arch:AVX2).
+*	Similarly, to enable more advanced AVX-512F instruction set option if the CPU processor supports it, go to:
+*	Project -> Fractals Properties -> Configuration Properties -> C/C++ -> Code Generation -> Enable Enhanced Instruction Set -> Advanced Vector Extensions 512 (/arch:AVX512).
+*	Please, make sure that it is properly set in both Debug and Release Configuration for the x64 platform.
+*/
 #if defined(__AVX512F__)
 		void calculateImmintrinsicAVX512(const ComplexRectangle& complexRectangle, const Rectangle<int>& screenRectangle) noexcept;
 		__m512i getIterationImintrin(const __m512d x, const __m512d y) const noexcept;
