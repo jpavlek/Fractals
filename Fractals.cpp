1d92a7c2f69c8de01d25d52770999db4f328b5d6
Jakov Pavlek <jakov.pavlek@gmail.com>
2021-04-17 05:57:37 +0200

    Add time benchmarking for different number of threads. Add FileSystem.h and FileSystem.cpp to the project filter file. Set C++17 as the language standard for the project and set EnableEnhancedInstructionSet: AdvancedVectorExtensions512.

--- Fractals.cpp
+++ Fractals.cpp
@@ -29,8 +29,35 @@ int main()
 #endif // DEBUG
 
 	ColorPalette colorPalette = ColorPaletteCreator::createPalette(ColorPalettesClass::RGBSpectrum10, 2);
+	std::cout << "\n";
+	
+	std::vector<ThreadCountBase> threadCountBases;
 
-	Fractals::Mandelbrot mandelbrot(width, height, colorPalette, maxIterations);
+	{
+		Timer timer;
+		timer.start();
+		Fractals::Mandelbrot mandelbrot(width, height, colorPalette, maxIterations, OperationMode::SingleThreaded, ThreadCountBase::ThreadCount_02x02_div_2);
+		std::cout << "\n";
+		timer.end();
+		std::cout << "\n";
+	}
+	
+	threadCountBases.emplace_back(ThreadCountBase::ThreadCount_02x02_div_2);
+	threadCountBases.emplace_back(ThreadCountBase::ThreadCount_04x04_div_2);
+	threadCountBases.emplace_back(ThreadCountBase::ThreadCount_08x08_div_2);
+	threadCountBases.emplace_back(ThreadCountBase::ThreadCount_16x16_div_2);
+	threadCountBases.emplace_back(ThreadCountBase::ThreadCount_32x32_div_2);
+	threadCountBases.emplace_back(ThreadCountBase::ThreadCount_64x64_div_2);
+
+	for (const auto threadCountBase : threadCountBases)
+	{
+		Timer timer;
+		timer.start();
+		Fractals::Mandelbrot mandelbrot(width, height, colorPalette, maxIterations, OperationMode::MultiThreaded, threadCountBase);
+		std::cout << "\n";
+		timer.end();
+		std::cout << "\n";
+	}
 
 	std::cout << "Done.\n";
 	timer.end();
