cc06fb7b4c0093ea5d42d10a314bc78fbc967ffa
Jakov Pavlek <jakov.pavlek@gmail.com>
2021-04-17 17:15:53 +0200

    Enable /Zc:__cplusplus compiler option to enable conditional code compilation for different versions of the C++ standard (14 support for filesystem (std::experimental::filesystem in <experimental/filesystem>) and std::filesystem in C++17.
    Enable AdvancedVectorExtensions2 (AVX2) instruction set by default.

--- FileSystem.h
+++ FileSystem.h
@@ -6,6 +6,12 @@ class FileSystem
 public:
 	static bool checkRelativeFilePath(std::string filename);
 
+/**
+*	By default __cplusplus variable in MS Visual Studio (2019) is set to pre-C++-11-standard value: 199711L (C++98 or C++03) for backward compatibility reasons.
+*	To get the correct value of __cplusplus in order to support different versions of C++ standard, compiler option /Zc:__cplusplus should be enabled.
+*	To enable /Zc:__cplusplus option, go to Project -> Fractals Properties -> Configuration Properties -> C/C++ -> Command Line -> Additional Options: /Zc:__cplusplus.
+*	Please, make sure that it is properly set in both Debug and Release Configuration, as well as for x64 (and Win32) platform(s).
+*/
 #if (__cplusplus < 201402L) //201103L (C++11) //199711L (C++98 or C++03) 
 	static bool checkFilePath(std::string filename);
 #endif
